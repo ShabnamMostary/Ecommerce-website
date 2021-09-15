b0e80a847632cc709b552227aacdc45da7e58fb4
Shabnam Mostary <shabnam@Shabnams-MBP.fios-router.home>
2021-09-15 10:48:36 -0400

    remove from cart action added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,5 +19,6 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   initialState, 
-  composeEnhancer(applyMiddleware(thunk)))
+  composeEnhancer(applyMiddleware(thunk))
+  )
 export default store
